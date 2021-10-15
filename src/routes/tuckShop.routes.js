59c8a54c547a0e9381a0242a2371c6f0801a2a15
johnojo97 <jojo@retrorabbit.co.za>
2021-10-16 00:10:06 +0200

    Cleaned up code

--- src/routes/tuckShop.routes.js
+++ src/routes/tuckShop.routes.js
@@ -5,9 +5,9 @@ const {
 module.exports = (app) => {
     app.post(GET_TOTAL, async (req, res) => {
         const selectedItems = req.body
-        console.log(selectedItems)
         let total = 0;
         // TODO: Calculate total of selected items and send total out of api
+
 
         res.status(200).send({total})
     });
