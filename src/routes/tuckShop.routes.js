97d2d2b322d8b2a06ea7af66d01a2387ec353b1e
BongumusaSizwe <1682836@students.wits.ac.za>
2021-10-16 15:35:35 +0200

    update

--- src/routes/tuckShop.routes.js
+++ src/routes/tuckShop.routes.js
@@ -2,13 +2,19 @@ const {
   GET_TOTAL
 } = require('../constants/endpointConstants');
 
+
 module.exports = (app) => {
     app.post(GET_TOTAL, async (req, res) => {
         const selectedItems = req.body
         let total = 0;
         // TODO: Calculate total of selected items and send total out of api
 
-
-        res.status(200).send({total})
+      for(let i=0;i<selectedItems.length;i++){
+        console.log(selectedItems[i].price);
+        total += selectedItems[i].price;
+      }
+      console.log(total);
+      
+      res.status(200).send({total})
     });
 }
